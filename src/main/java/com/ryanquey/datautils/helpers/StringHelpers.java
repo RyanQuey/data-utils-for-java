3d3c30e7f3e2c8041c3cc9bb64d695fef0138978
Ryan Quey <quey.ryan@gmail.com>
2020-09-07 14:39:38 -0700

    add more string conversion helpers

--- src/main/java/com/ryanquey/datautils/helpers/StringHelpers.java
+++ src/main/java/com/ryanquey/datautils/helpers/StringHelpers.java
@@ -9,10 +9,21 @@ import org.json.JSONException;
 import com.google.common.base.CaseFormat;
 
 
-
 public class StringHelpers {
   public static String snakeToCamel(String str) {
     return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, str); 
+  }
+
+  public static String snakeToUpperCamel(String str) {
+    return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, str); 
+  }
+
+  public static String camelToSnake(String str) {
+    return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, str); 
+  }
+
+  public static String upperCamelToSnake(String str) {
+    return CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, str); 
   }
 }
 
