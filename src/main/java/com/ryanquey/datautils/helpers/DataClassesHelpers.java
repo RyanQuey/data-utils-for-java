012a48f3377594ec531f0d14cda61154df42a67b
Ryan Quey <quey.ryan@gmail.com>
2020-09-12 19:15:09 +0000

    add error handling for models

--- src/main/java/com/ryanquey/datautils/helpers/DataClassesHelpers.java
+++ src/main/java/com/ryanquey/datautils/helpers/DataClassesHelpers.java
@@ -6,7 +6,7 @@ import java.lang.IllegalAccessException;
 public class DataClassesHelpers {
 	// copies from objA to objB
 	// https://stackoverflow.com/a/8132962/6952495
-  static public void copyMatchingFields (Object objA, Object objB) {
+  static public void copyMatchingFields (Object objA, Object objB) throws Exception {
     Class objAClass = objA.getClass();
     Class objBClass = objB.getClass();
     Method[] methods = objAClass.getMethods();
@@ -15,9 +15,11 @@ public class DataClassesHelpers {
       String methodName = methods[i].getName();
 
       try {
-        if (methodName.startsWith("get")){
+        // don't want to try doing objA.setClass() ...
+        if (methodName.startsWith("get") && methodName != "getClass"){
           Class returnType = methods[i].getReturnType();
           Method setter = objBClass.getMethod(methodName.replaceFirst("get", "set"), returnType);
+          // System.out.println("calling " + methodName);
           Method getter = methods[i];
 
           // get from objA and then set it on objB
@@ -25,11 +27,15 @@ public class DataClassesHelpers {
         }
 
       } catch (IllegalAccessException e) {
+        throw e;
       } catch (InvocationTargetException e) {
+        throw e;
       } catch (NoSuchMethodException e) {
-        // TODO: handle exception...or do nothing probably
+        
+          throw e;
       } catch (IllegalArgumentException e) {
         // TODO: handle exception...or do nothing probably
+        throw e;
       }
     }
   }
