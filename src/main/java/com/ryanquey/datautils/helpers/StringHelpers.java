a98d3def3c8783a14d0406a9d32de323d6b172a1
Ryan Quey <quey.ryan@gmail.com>
2020-11-07 16:21:24 -0800

    add more string helpers; fix some of their names

--- src/main/java/com/ryanquey/datautils/helpers/StringHelpers.java
+++ src/main/java/com/ryanquey/datautils/helpers/StringHelpers.java
@@ -11,19 +11,50 @@ import com.google.common.base.CaseFormat;
 
 
 public class StringHelpers {
+  //////////////////////////////
+  // STRING CONVERSION
+  //////////////////////////////
+
+  //////////
+  // from lower snake
+  //////////
   public static String snakeToCamel(String str) {
-    return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, str); 
+    return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, str); 
   }
 
   public static String snakeToUpperCamel(String str) {
+    return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, str); 
+  }
+
+  //////////
+  // from upper snake
+  //////////
+  public static String upperSnakeToCamel(String str) {
+    return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, str); 
+  }
+
+  public static String upperSnakeToUpperCamel(String str) {
     return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, str); 
   }
 
+  //////////
+  // from camel case
+  //////////
   public static String camelToSnake(String str) {
+    return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, str); 
+  }
+
+  public static String camelToUpperSnake(String str) {
     return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, str); 
   }
 
+  //////////
+  // from upper camel case
+  //////////
   public static String upperCamelToSnake(String str) {
+    return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, str); 
+  }
+  public static String upperCamelToUpperSnake(String str) {
     return CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, str); 
   }
 }
